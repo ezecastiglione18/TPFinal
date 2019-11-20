c3199c21800d4079dbd909569b99945e1810c90f
ezecastiglione18 <BocaVoley18>
2019-11-19 22:47:27 -0300

    Algunas cosas basicas. Ver errores marcados

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,3 +1,45 @@
+import java.awt.List
+
+class Personaje(var energia: Int){
+    def felicidad(): Int ={
+
+    } //COMO SACO EL UNIT?
+
+  def rebeldia() = 1 / this.felicidad()
+
+  def esRebelde(): Boolean ={return this.rebeldia() > 10}
+
+  } //Y ESTE ERROR????
+}
+
+class Huesped (energia: Int, var minutosRestantes: Int, var amigos: List[Personaje]) extends Personaje (energia) {
+  override def felicidad(): Int = {
+    return minutosRestantes * this.felicidadAmigos()
+  }
+
+  def felicidadAmigos(): Int = {} //ACA TAMBIEN. COMO SACO EL UNIT?
+}
+
+
+class Anfitrion (energia: Int, var velocidadProcesamiento: Int, var recuerdos: List[Recuerdo]) extends Personaje (energia){
+      override def felicidad(): Int = {
+      return energia / velocidadProcesamiento * 10
+    }
+}
+
+class Recuerdo (var descripcion: String, var escenario: Escenario){
+
+}
+
+class Escenario (var nombre: String, var categoria: String){
+
+}
+
+class Trama (var persoanjes: List[Personaje], var escenario: Escenario){
+
+}
+
+
 /*
 class Personaje{
   var energia
